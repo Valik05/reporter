f10759f49d2ecfb8fe97ae54f371f6e962a4a871
Valentin <chinchik38@gmail.com>
2025-05-21 18:35:57 +0200

    add closing sidemenu after choosing route

--- src/components/UI/LinksList/LinksList.tsx
+++ src/components/UI/LinksList/LinksList.tsx
@@ -1,14 +1,17 @@
 import './linksList.css';
 import classNames from 'classnames';
 import CustomLink from '../CustomLink/CustomLink';
+import { useSideMenu } from '../../../context/useSideMenu';
 import type { NavLink } from '../../../models/NavLinks/NavLinks';
 
 const LinksList = ({ links }: { links: NavLink[] }) => {
+    const { setSideMenu } = useSideMenu();
     return (
         <nav className={classNames("links-list-container")}>
             {links.map((item, index) =>
                 <CustomLink
                     {...item}
+                    onClick={() => setSideMenu(null)}
                     style={{ flexDirection: "row-reverse", fontSize: "1.75rem" }}
                     key={index}
                 />
