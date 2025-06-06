2c8735f62144d0b353b3774f1a52fc5868b60e77
Valentin <chinchik38@gmail.com>
2025-06-06 19:17:57 +0200

    update sdk key

--- src/context/useSdk.tsx
+++ src/context/useSdk.tsx
@@ -18,7 +18,7 @@ export const SdkProvider = ({ children }: Props) => {
     const initializeSdk = useCallback(() => {
         if (window.TrafficGramSDK) {
             window.trafficGramSDK = window.TrafficGramSDK.initialize(
-                "b1ltcY0yc0sqzWeFUebAiy69emSxidtKEtsS09BAzHZDJDKYh5BrZFnhn2qb0hhW"
+                "RU9avyMHzBInI3Q1RBBSnBmRxpgKHOpu7YYJSgBx9q2ybgpgmSquO0QzmeTNvBi5"
             );
             console.log("SDK initialized");
         } else {
